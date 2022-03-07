e48694d853f02642a149b4257a6f782b59125014
Drew Dahlquist <drewdahlquistdev@gmail.com>
2022-03-06 22:20:42 -0600

    Probably approximately done

--- project/src/client/client.py
+++ project/src/client/client.py
@@ -7,8 +7,85 @@ March 18, 2022
 TODO: Program description
 """
 
+"""
+something helpful...
+lsof -i tcp:10970
+"""
+
 import socket
 
-HOST="127.0.0.1"
+HOST='127.0.0.1'
 PORT=10970
 
+loggedin = False
+
+print()
+print('My chat room client. Version One.')
+print()
+
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        cmd = input()
+
+        parsed = cmd.split()
+
+        if(parsed[0] == 'login'):
+            if(loggedin == False and len(parsed) == 3):
+                s.send(cmd.encode())
+                data = s.recv(1024)
+                # print(f'{data.decode()!r}')
+                # success
+                if(data.decode().startswith('Ack:')):
+                    loggedin = True
+                    print('login confirmed')
+                else:
+                    print('Denied. User name or password incorrect.')
+            # err
+            else:
+                print('Err:Client: Not logged out.')
+
+        elif(parsed[0] == 'newuser'):
+            if(loggedin == False and len(parsed) == 3 and len(parsed[1]) >= 3 and len(parsed[1]) <= 32 and len(parsed[2]) >= 4 and len(parsed[2]) <= 8):
+                s.send(cmd.encode())
+                data = s.recv(1024)
+                # print(f'{data.decode()!r}')
+                # success
+                if(data.decode().startswith('Ack:')):
+                    print('New user account created. Please login.')
+                # err
+                else:
+                    print('Denied. User account already exists.')
+            # err
+            else:
+                print('Err:Client: Not logged out.')
+
+        
+        elif(parsed[0] == 'send'):
+            if(loggedin == True and len(parsed) >= 2 and len(' '.join(parsed[1:])) >= 1 and len(' '.join(parsed[1:])) <= 256):
+                s.send(cmd.encode())
+                data = s.recv(1024)
+                if(data.decode().startswith('Ack:')):
+                    print(data.decode().split(':')[2:])
+                else:
+                    pass
+            # err
+            else:
+                print('Denied. Please login first.')
+
+        elif(parsed[0] == 'logout'):
+            if(loggedin == True and len(parsed) == 1):
+                s.send(cmd.encode())
+                data = s.recv(1024)
+                if(data.decode().startswith('Ack:')):
+                    print(data.decode().split(':')[2:])
+                else:
+                    pass
+                s.close()
+                loggedin = False
+            # err
+            else:
+                print('Err:Client: Not logged in.')
+        
+        else:
+            print('Err:Client: Invalid command. Try again.')
--- project/src/server/server.py
+++ project/src/server/server.py
@@ -7,8 +7,85 @@ March 18, 2022
 TODO: Program description
 """
 
+"""
+something helpful...
+lsof -i tcp:10970
+"""
+
 import socket
 
-HOST="127.0.0.1"
+HOST='127.0.0.1'
 PORT=10970
 
+online = str()
+users = dict()
+
+print()
+print('My chat room server. Version One.')
+print()
+
+# read in users.txt on startup
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Server listening on {HOST}:{PORT}")
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                # conn.sendall(data)
+
+                cmd = str(data.decode())
+                parsed = cmd.split()
+                
+                if(parsed[0] == 'login' and len(parsed) == 3):
+                    # valid user & pass
+                    if(parsed[1] in users.keys() and parsed[2] == users[parsed[1]]):
+                        online = parsed[1]
+                        ack = f'Ack:Server: Logged on as {online}.'
+                        conn.send(ack.encode())
+                        print(f'{online} login.')
+                    # error
+                    else:
+                        err = f'Err:Server: Invalid username or password.'
+                        conn.send(err.encode())
+
+                if(parsed[0] == 'newuser' and len(parsed) == 3):
+                    # user creation
+                    if(parsed[1] not in users.keys()):
+                        users[parsed[1]] = parsed[2]
+                        ack = f'Ack:Server: New user created with username: {parsed[1]}, password: {parsed[2]}.'
+                        conn.send(ack.encode())
+                        print('New user account created.')
+                    # user already exists
+                    else:
+                        err = f'Err:Server: User already exists.'
+                        conn.send(err.encode())
+                
+                if(parsed[0] == 'send' and len(parsed) >= 2):
+                    # logged in
+                    if(online != ''):
+                        ack = f'{online}: {" ".join(parsed[1:])}.'
+                        conn.sendall(ack.encode())
+                        print(f'{online}: {" ".join(parsed[1:])}.')
+                    # not logged in
+                    else:
+                        err = f'Err:Server: Not logged in.'
+                        conn.send(err.encode())
+
+                if(parsed[0] == 'logout'):
+                    # logged in
+                    if(online != ''):
+                        ack = f'Ack:Server: {online} left.'
+                        conn.send(ack.encode())
+                        print(f'{online} logout.')
+                        online = ''
+                    # not logged in
+                    else:
+                        err = f'Err:Server: Not logged in.'
+                        conn.send(err.encode())
