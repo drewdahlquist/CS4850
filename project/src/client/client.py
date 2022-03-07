ac1c8cc067e855b9f6d8fd5c2129d664094aa320
Drew Dahlquist <drewdahlquistdev@gmail.com>
2022-03-07 00:04:03 -0600

    Project looks entirely done. All behaviors/output implemented.

--- project/src/client/client.py
+++ project/src/client/client.py
@@ -14,9 +14,11 @@ lsof -i tcp:10970
 
 import socket
 
+# constant vars for connection
 HOST='127.0.0.1'
 PORT=10970
 
+# state var for whether user is logged in or not
 loggedin = False
 
 print()
@@ -24,69 +26,83 @@ print('My chat room client. Version One.')
 print()
 
 while True:
+    # some standard socket setup
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        cmd = input()
 
-        parsed = cmd.split()
+        cmd = input() # user command
+        parsed = cmd.split() # for parsing/processing only
 
+        # handle login logic
         if(parsed[0] == 'login'):
+            # correct usage
             if(loggedin == False and len(parsed) == 3):
                 s.send(cmd.encode())
                 data = s.recv(1024)
-                # print(f'{data.decode()!r}')
                 # success
                 if(data.decode().startswith('Ack:')):
-                    loggedin = True
+                    loggedin = True # update state var
                     print('login confirmed')
+                # error
                 else:
                     print('Denied. User name or password incorrect.')
-            # err
+            # incorrect usage
             else:
                 print('Err:Client: Not logged out.')
 
+        # handle newuser logic
         elif(parsed[0] == 'newuser'):
+            # correct usage
             if(loggedin == False and len(parsed) == 3 and len(parsed[1]) >= 3 and len(parsed[1]) <= 32 and len(parsed[2]) >= 4 and len(parsed[2]) <= 8):
                 s.send(cmd.encode())
                 data = s.recv(1024)
-                # print(f'{data.decode()!r}')
                 # success
                 if(data.decode().startswith('Ack:')):
                     print('New user account created. Please login.')
-                # err
+                # error
                 else:
                     print('Denied. User account already exists.')
-            # err
+            # incorrect usage
             else:
                 print('Err:Client: Not logged out.')
 
-        
+        # handle send logic
         elif(parsed[0] == 'send'):
+            # correct usage
             if(loggedin == True and len(parsed) >= 2 and len(' '.join(parsed[1:])) >= 1 and len(' '.join(parsed[1:])) <= 256):
                 s.send(cmd.encode())
                 data = s.recv(1024)
+                # success
                 if(data.decode().startswith('Ack:')):
                     msg = data.decode().split(':')[2]+':'
                     print(msg+''.join(data.decode().split(':')[3:]))
+                # error
                 else:
                     pass
-            # err
+            # incorrect usage
             else:
                 print('Denied. Please login first.')
 
+        # handle logout logic
         elif(parsed[0] == 'logout'):
+            # correct usage
             if(loggedin == True and len(parsed) == 1):
                 s.send(cmd.encode())
                 data = s.recv(1024)
+                # success
                 if(data.decode().startswith('Ack:')):
                     print(''.join(data.decode().split(':')[2:]))
+                # error
                 else:
                     pass
+                # close socket
                 s.close()
-                loggedin = False
-            # err
+                loggedin = False # update state var
+                exit() # exit program
+            # incorrect usage
             else:
                 print('Err:Client: Not logged in.')
         
+        # command not recognized
         else:
             print('Err:Client: Invalid command. Try again.')
